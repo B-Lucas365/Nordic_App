a298641f43b238dba6045a87b289bb3cc6d38475
Lucas Renan <lucasrenan365@outlook.com>
2026-02-10 10:03:01 -0300

    Initial Commit

---
+++ Nordic/NordicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NordicApp.swift
+//  Nordic
+//
+//  Created by Lucas Renan on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct NordicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
